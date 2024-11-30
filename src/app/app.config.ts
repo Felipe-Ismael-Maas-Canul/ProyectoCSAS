980f94e0279e27c54f5162da90c7a77d637735cf
natyarrazola <7491@itescam.edu.mx>
2024-11-30 12:24:46 -0600

    confg Httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch  } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,7 +8,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(), // Configura el HttpClient aquí
+    provideHttpClient(withFetch()), // Configura el HttpClient y  Habilita fetch
     provideRouter(routes),
     provideClientHydration()
   ]
